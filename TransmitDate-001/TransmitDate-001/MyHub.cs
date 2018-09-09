779a72da6f1200071deff88c8133c11f16c2be2a
Leonardo <leoo.rossetti@gmail.com>
2018-09-09 19:00:52 +1200

    Initial commit

---
+++ TransmitDate-001/TransmitDate-001/MyHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace TransmitDate_001
+{
+
+    /*
+     * NUGET: install-package Microsoft.aspnet.signalr
+     */
+
+    public class MyHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void BroadCastServerTime()
+        {
+            Clients.All.DateReciever(DateTime.Now);
+        }
+
+        public void BroadCastServerMessage(string message)
+        {
+            Clients.All.MessageReciever(message);
+        }
+    }
+}
